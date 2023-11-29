182fbe2e7d7096b745308e18042fda641d1c93c7
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-11-29 10:32:00 -0600

    created an app file uninstalled gluestack and installed react native paper

---
+++ app/App.js
@@ -0,0 +1,22 @@
+import SafeAreaView from 'react-native-safe-area-view';
+import { View, Text, ScrollView, Image } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { useTheme } from 'react-native-paper';
+import images from '../assets/images';
+
+export default function Home() {
+    const theme = useTheme()
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: theme.colors.tertiary }}>
+        <Text>
+          Look, I'm safe! Not under a status bar or notch or home indicator or
+          anything! Very cool
+        </Text>
+        <Image source={images.challah} style={{ width: 200, height: 200 }}/>
+        <Image source={images.magendavid} style={{ width: 200, height: 200 }}/>
+        <Image source={images.kotel} style={{ width: 200, height: 200 }}/>
+      </View>
+    </SafeAreaView>
+  );
+}
--- app/index.js
+++ app/index.js
@@ -1,65 +1,19 @@
-// import SafeAreaView from 'react-native-safe-area-view';
-// import { View, Text, ScrollView, Image } from 'react-native';
-// import { Stack, useRouter } from 'expo-router';
-// import images from '../assets/images';
-// // import carousel from 'carousel';
-// // import "bootstrap";
-// // import "bootstrap/dist/css/bootstrap.min.css";
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import Home from './App';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#663399',
+    secondary: 'yellow',
+  },
+};
 
-// export default function Home() {
-//   return (
-//     <SafeAreaView style={{ flex: 1 }}>
-//       <View style={{ flex: 1 }}>
-//         <Text>
-//           Look, I'm safe! Not under a status bar or notch or home indicator or
-//           anything! Very cool
-//         </Text>
-//         <Image source={images.challah} style={{ width: 200, height: 200 }}/>
-//         <Image source={images.magendavid} style={{ width: 200, height: 200 }}/>
-//         <Image source={images.kotel} style={{ width: 200, height: 200 }}/>
-//       </View>
-//       {/* <View> */}
-//         {/* <Carousel /> */}
-//       {/* </View> */}
-//     </SafeAreaView>
-//   );
-// }
-
-
-// import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
-// import { config } from '@gluestack-ui/config'; // Optional if you want to use default theme
-
-// export default function Home() {
-//   return (
-//     <GluestackUIProvider config={config}>
-//       <Text>Hello World!</Text>
-//     </GluestackUIProvider>
-//   );
-// }
-
-import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
-import { config } from '@gluestack-ui/config'; // Optional if you want to use default theme
-import SafeAreaView from 'react-native-safe-area-view';
-import { View, ScrollView, Image } from 'react-native';
-import { Stack, useRouter } from 'expo-router';
-import images from '../assets/images';
-
-export default function Home() {
+export default function Main() {
   return (
-    <GluestackUIProvider config={config}>
-     <SafeAreaView style={{ flex: 1 }}>
-       <View style={{ flex: 1 }}>
-         <Text>
-           Look, I'm safe! Not under a status bar or notch or home indicator or
-           anything! Very cool
-         </Text>
-         <Image source={images.challah} style={{ width: 200, height: 200 }}/>
-         <Image source={images.magendavid} style={{ width: 200, height: 200 }}/>
-         <Image source={images.kotel} style={{ width: 200, height: 200 }}/>
-       </View>
-     </SafeAreaView>
-      <Text>Hello World!</Text>
-    </GluestackUIProvider>
+    <PaperProvider theme={theme}>
+      <Home />
+    </PaperProvider>
   );
-}
+};
