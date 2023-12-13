34334e472b0d9cfb67b666cd0dedac0f45760116
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-12-13 10:49:30 -0600

    add additional tabs

--- app/tabs.js
+++ app/tabs.js
@@ -9,10 +9,30 @@ const FirstRoute = () => (
 const SecondRoute = () => (
   <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
 );
+const ThirdRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
+const FourthRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
+const FifthRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
+const SixthRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
+const SeventhRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
 
 const renderScene = SceneMap({
   first: FirstRoute,
   second: SecondRoute,
+  third: ThirdRoute,
+  fourth: FourthRoute,
+  fifth: FifthRoute,
+  sixth: SixthRoute,
+  seventh: SeventhRoute,
 });
 
 export default function Tabs() {
@@ -20,8 +40,13 @@ export default function Tabs() {
 
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'first', title: 'First' },
-    { key: 'second', title: 'Second' },
+    { key: 'first', title: 'About Us' },
+    { key: 'second', title: 'Events' },
+    { key: 'third', title: 'Ignite the Light' },
+    { key: 'fourth', title: 'Trips' },
+    { key: 'fifth', title: 'Chevra House' },
+    { key: 'sixth', title: 'Donate' },
+    { key: 'seventh', title: 'Contact Us' },
   ]);
 
   return (
@@ -33,3 +58,4 @@ export default function Tabs() {
     />
   );
 }
+
