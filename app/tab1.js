b88432d9144071152d60b1bd80c201551f538e59
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2023-12-25 11:40:44 -0600

    outlined option buttons

--- app/tab1.js
+++ app/tab1.js
@@ -1,19 +1,35 @@
 import * as React from 'react';
-import { Avatar, Button, Card, Text } from 'react-native-paper';
+import { Avatar, Card, Text, Button } from 'react-native-paper';
+import { ScrollView, View, FlatList } from 'react-native';
 
 const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 const Tab1 = () => (
-    <Card>
-    <Card.Actions>
-      <Button>Who We Are</Button>
-      <Button>Our History</Button>
-      <Button>Staff Bios</Button>
-      <Button>Affiliate Partners</Button>
-    </Card.Actions>
-  </Card>
-);
+    <ScrollView>
+        <View>
+        <Card>
+        <Card.Actions  style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Button onPress={() => console.log('Pressed')} mode= "outlined">
+                Who We Are
+            </Button>
+            <Button onPress={() => console.log('Pressed')} mode= "outlined">
+                What We Do
+            </Button>
+            <Button onPress={() => console.log('Pressed')} mode= "outlined">
+                Our History
+            </Button>
+            <Button onPress={() => console.log('Pressed')} mode= "outlined">
+                Staff Bios
+            </Button>
+            <Button onPress={() => console.log('Pressed')} mode= "outlined">
+                Affiliate Partners
+            </Button>
+         </Card.Actions>
+         </Card>
+         </View>
+    </ScrollView>
+  );
+  
+  export default Tab1;
 
-
-export default Tab1;
 
