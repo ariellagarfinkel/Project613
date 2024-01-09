735ee36bf92abfe0135a011704e6ca8a44cd9ae1
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2024-01-09 11:25:56 -0600

    start header again

--- app/topbar.js
+++ app/topbar.js
@@ -3,15 +3,8 @@ import { Appbar, Colors } from 'react-native-paper';
 // import images from '../assets/images';
 
 const TopBar = () => {
-  // const _goBack = () => console.log('Went back');
-
-  // const _handleSearch = () => console.log('Searching');
-
-  // const _handleMore = () => console.log('Shown more');
-  // const lighterBlueColor = Colors.blue300;
   return (
     <Appbar.Header>
-      {/* <Appbar.BackAction onPress={_goBack} /> */}
       <Appbar.Content 
         title="Project613"
         style={{
@@ -19,8 +12,6 @@ const TopBar = () => {
           justifyContent: 'center',
           backgroundColor: 'blue',
           }} />
-      {/* <Appbar.Action icon="magnify" onPress={_handleSearch} /> */}
-      {/* <Appbar.Action icon="dots-vertical" onPress={_handleMore} /> */}
     </Appbar.Header>
   );
 };
